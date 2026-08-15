0f9f3c6a036e0da19a9512d8c7e8112f90b61f17
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-08-16 03:28:45 +0545

    Rename folders and upload Screenshots

--- Operating System/OS_LABS/1/read_write_std.c
+++ Operating System/OS_LABS/1/read_write_std.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 int main() {
 	char buffer[10];
 
 	write(1, "Enter 10 bytes of data: ", 24);
 	read(0, buffer, 10);
+
+	// Flush any leftover input so it doesn't leak into the shell
+	tcflush(0, TCIFLUSH);
 
 	write(1, "Stored buffer: ", 15);
 	write(1, buffer, 10);
